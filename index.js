4eb8b3032df6ef3de8f638354cc250715bba5330
Alex-Viet <alex.night.power@gmail.com>
2023-06-07 22:31:10 +0300

    Форма входа вынесена в отдельный компонент

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { addTodo, deleteTodo, getTodo } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js";
 
 let tasks = [];
 
@@ -17,31 +18,14 @@ const renderApp = () => {
     const appEl = document.getElementById("app");
 
     if (!token) {
-        const appHtml = `<h1>Список задач</h1>
-                        <div class="form">
-                            <h3 class="form-title">Форма входа</h3>
-                            <div class="form-row">
-                                Логин:
-                                <input type="text" id="login-input" class="input" />
-                                <br/>
-                                Пароль:
-                                <input type="text" id="login-input" class="input" />
-                            </div>
-                            <br />
-                            <button class="button" id="login-button">Войти</button>
-                        </div>`
-
-        appEl.innerHTML = appHtml;
-
-        const loginButtonElem = document.getElementById("login-button");
-
-        loginButtonElem.addEventListener("click", () => {
-            token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-            fetchTodosAndRender();
+        renderLoginComponent({
+            appEl,
+            setToken: (newToken) => { token = newToken },
+            fetchTodosAndRender,
         });
 
         return;
-    }
+    };
 
     const tasksHtml = tasks
         .map((task) => {
